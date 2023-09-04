5383379a821de3567944415a8cd5afd4534d88bf
RiceMCUT <2950767658@qq.com>
2023-09-04 22:37:52 +0800

    Make hook RL_ClientReady use gameevent player_activate some cleaning in progress riceui element

--- lua/ricelib_preload/sh_hooks.lua
+++ lua/ricelib_preload/sh_hooks.lua
@@ -1,17 +1,6 @@
 if SERVER then
-    util.AddNetworkString("RL_ClientReady")
-
-    net.Receive("RL_ClientReady", function(_, ply)
-        hook.Run("RL_ClientReady", ply)
-    end)
-else
-    hook.Add("InitPostEntity", "RL_ClientReady", function()
-        net.Start("RL_ClientReady")
-        net.SendToServer()
-    end)
-
-    concommand.Add("RL_Hooks_FakeReadyMessage", function()
-        net.Start("RL_ClientReady")
-        net.SendToServer()
-    end)
+    gameevent.Listen( "player_activate" )
+    hook.Add( "player_activate", "RL_ClientReady", function( data )
+        hook.Run("RL_ClientReady", Player(data.userid))
+    end )
 end
--- lua/riceui/elements/progress.lua
+++ lua/riceui/elements/progress.lua
@@ -1,7 +1,7 @@
 local Element = {}
 
-function Element.Create(data,parent)
-    RL.table.Inherit(data,{
+function Element.Create(data, parent)
+    RL.table.Inherit(data, {
         x = 10,
         y = 10,
         w = 300,
@@ -9,23 +9,21 @@ function Element.Create(data,parent)
         Time = 0.25
     })
 
-    local panel = vgui.Create("DProgress",parent)
-    panel:SetPos(RL.hudScale(data.x,data.y))
-    panel:SetSize(RL.hudScale(data.w,data.h))
+    local panel = vgui.Create("DProgress", parent)
+    panel:SetPos(RL.hudScale(data.x, data.y))
+    panel:SetSize(RL.hudScale(data.w, data.h))
     panel.GThemeType = "Panel"
     panel:SetFraction(0)
+    RiceUI.Smooth_CreateController(panel, data.Time)
 
-    RiceUI.Smooth_CreateController(panel,data.Time)
-
-    panel.Paint=function(self,w,h)
-        surface.SetDrawColor(0,0,0,100)
-        surface.DrawRect(0,0,w,h)
-
-        surface.SetDrawColor(0,255,0)
-        surface.DrawRect(0,0,w*RiceUI.Smooth(self,self:GetFraction()),h)
+    panel.Paint = function(self, w, h)
+        surface.SetDrawColor(0, 0, 0, 100)
+        surface.DrawRect(0, 0, w, h)
+        surface.SetDrawColor(0, 255, 0)
+        surface.DrawRect(0, 0, w * RiceUI.Smooth(self, self:GetFraction()), h)
     end
 
-    RiceUI.MergeData(panel,RiceUI.ProcessData(data))
+    RiceUI.MergeData(panel, RiceUI.ProcessData(data))
 
     return panel
 end
