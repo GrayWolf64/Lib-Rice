e7bc292f9ae3e1d1078da728c9310ef29b5d8817
GrayWolf <tairikuookami@foxmail.com>
2023-11-18 09:51:14 +0800

    Combined 2 files related to pre-registration into a single one

--- lua/ricelib/cl_preregister.lua
+++ lua/ricelib/cl_preregister.lua
@@ -1,3 +1,39 @@
+RL.VGUI.RegisterFontAdv("DS-Digital", "Digi")
+RL.VGUI.RegisterFontAdv("OPlusSans 3.0", "OPSans_L")
+RL.VGUI.RegisterFontAdv("OPlusSans 3.0 Medium", "OPSans")
+RL.VGUI.RegisterFontAdv("Source Han Sans SC", "SourceHan")
+RL.VGUI.RegisterFontAdv("OPlusSans 3.0 Medium", "OPPOSans")
+RL.VGUI.RegisterFontFixed("OPlusSans 3.0 Medium", "OPPOSans_F")
+
+RL.VGUI.RegisterFont_New{
+    CodeName = "RiceUI",
+}
+
+RL.VGUI.RegisterFont_New{
+    CodeName = "RiceUI_M",
+    FontName = "OPlusSans 3.0 Medium"
+}
+
+RL.VGUI.RegisterFont_New{
+    CodeName = "RiceUI_B",
+    FontName = "OPlusSans 3.0 Bold"
+}
+
+RL.VGUI.RegisterFont_New{
+    CodeName = "Source",
+    FontName = "Source Han Sans SC"
+}
+
+RL.VGUI.RegisterFont_New{
+    CodeName = "Smiley",
+    FontName = "Smiley Sans Oblique"
+}
+
+RL.VGUI.RegisterFont_New{
+    CodeName = "Ali",
+    FontName = "Alimama ShuHeiTi"
+}
+
 RL.VGUI.RegisterColorTheme("Dark1", {Color(40, 40, 40, 255), Color(60, 60, 60, 255), Color(250, 250, 250), Color(60, 60, 60)})
 RL.VGUI.RegisterColorTheme("Dark2", {Color(50, 50, 50, 255), Color(70, 70, 70, 255), Color(250, 250, 250), Color(70, 70, 70)})
 RL.VGUI.RegisterColorTheme("Dark3", {Color(60, 60, 60, 255), Color(80, 80, 80, 255), Color(250, 250, 250), Color(80, 80, 80)})
