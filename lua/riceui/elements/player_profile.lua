6750f901331ba1a82b579acb50081c93f69c3e6d
RiceMCUT <2950767658@qq.com>
2024-04-05 17:40:17 +0800

    added render scissor for shadow rendering added Widget system for RiceUI to replace prefabs

--- lua/riceui/elements/player_profile.lua
+++ lua/riceui/elements/player_profile.lua
@@ -1,5 +1,53 @@
 local Element = {}
-function Element.Create(data,parent)
+
+local classes = {
+    Default = function(data, parent)
+        local panel = vgui.Create("AvatarImage", parent)
+        panel:SetPos(RiceLib.hudScale(data.x, data.y))
+        panel:SetSize(RiceLib.hudScale(data.w, data.h))
+        panel:SetPlayer(data.ply or data.Player, data.w)
+
+        return panel
+    end,
+
+    Rounded = function(data, parent)
+        local panel = RiceUI.SimpleCreate({type = "rl_panel",
+            NoGTheme = true,
+            Theme = {},
+
+            w = data.w,
+            h = data.h,
+
+            Paint = function(self, w, h)
+                RiceLib.Render.StartStencil()
+
+                RiceLib.Draw.RoundedBox(8, 0, 0, w, h, color_white)
+
+                render.SetStencilCompareFunction(STENCIL_EQUAL)
+                render.SetStencilFailOperation(STENCIL_KEEP)
+
+                self.Picture:SetSize(w, h)
+                self.Picture:PaintManual()
+
+                render.SetStencilEnable(false)
+            end,
+        }, parent, parent)
+
+        function panel:OnRemove()
+            self.Picture:Remove()
+        end
+
+        local picture = vgui.Create("AvatarImage", panel)
+        picture:SetPaintedManually(true)
+        picture:SetPlayer(data.ply or data.Player, data.w)
+
+        panel.Picture = picture
+
+        return panel
+    end,
+}
+
+function Element.Create(data, parent)
     RiceLib.table.Inherit(data,{
         x = 10,
         y = 10,
@@ -8,19 +56,9 @@ function Element.Create(data,parent)
         ShadowAlpha = 0,
     })
 
-    local panel = vgui.Create("AvatarImage",parent)
-    panel:SetPos(RiceLib.hudScale(data.x,data.y))
-    panel:SetSize(RiceLib.hudScale(data.w,data.h))
-    panel:SetPlayer(data.ply or data.Player,data.w)
+    local panel = classes[data.Class or "Default"](data, parent)
 
-    --local old_Paint = panel.Paint
-    function panel:Paint()
-        RiceUI.Render.DrawShadowEx(self.ShadowAlpha,self,true,true,true,true)
-
-        --old_Paint()
-    end
-
-    RiceUI.MergeData(panel,RiceUI.ProcessData(data))
+    RiceUI.MergeData(panel, RiceUI.ProcessData(data))
 
     return panel
 end
--- lua/riceui/elements/rl_button.lua
+++ lua/riceui/elements/rl_button.lua
@@ -1,4 +1,4 @@
-local Element = {}
+    local Element = {}
 Element.Editor = {Category = "interact"}
 function Element.Create(data,parent)
     RiceLib.table.Inherit(data,{
@@ -23,6 +23,27 @@ function Element.Create(data,parent)
         if self:GetParent().RiceUI_Event then
             self:GetParent():RiceUI_Event("Button_Click",self.ID,self)
         end
+    end
+
+    function panel:OnMouseReleased()
+        self.Depressed = false
+
+        self:MouseCapture(false)
+    end
+
+    function panel:OnMousePressed(keyCode)
+        if keyCode == MOUSE_LEFT then
+            if self.ClickSound then surface.PlaySound(self.ClickSound) end
+
+            self:DoClick()
+        end
+
+        if keyCode == MOUSE_RIGHT then self:DoRightClick() end
+        if keyCode == MOUSE_MIDDLE then self:DoMiddleClick() end
+
+        self.Depressed = true
+
+        self:MouseCapture(true)
     end
 
     RiceUI.MergeData(panel,RiceUI.ProcessData(data))
