18d5feb36114095aba0552288012ca6ed6bcdc10
GrayWolf <tairikuookami@foxmail.com>
2024-08-01 11:13:38 +0800

    Cleaned table lib for further improvements

--- lua/ricelib_preload/sh_!table.lua
+++ lua/ricelib_preload/sh_!table.lua
@@ -1,70 +1,57 @@
 RiceLib.table = RiceLib.table or {}
 
-function RiceLib.table.Inherit( t, base, override, blacklist )
-	override = override or {}
+local pairs = pairs
+
+local function inherit(t, base, override, blacklist)
+	override  = override or {}
 	blacklist = blacklist or {}
 
-	for k, v in pairs( base ) do
+	for k, v in pairs(base) do
 		if blacklist[k] then
 			continue
 		end
 
 		if override[k] then
-			t[ k ] = v
+			t[k] = v
 		end
 
-		if ( t[ k ] == nil ) then t[ k ] = v end
+		if t[k] == nil then t[k] = v end
 	end
 
 	return t
-
 end
 
-function RiceLib.table.InheritCopy( t, base, override, blacklist )
-	local tbl = table.Copy(t)
+local function inherit_copy(t, base, override, blacklist)
+	return inherit(table.Copy(t), table.Copy(base), override, blacklist)
+end
 
-	override = override or {}
-	blacklist = blacklist or {}
+local function get_by_str(str)
+	local t = _G
 
-	for k, v in pairs( table.Copy(base) ) do
-		if blacklist[k] then
-			continue
-		end
-
-		if override[k] then
-			tbl[ k ] = v
-		end
-
-		if ( tbl[ k ] == nil ) then tbl[ k ] = v end
+	for _, v in ipairs(str:Split(".")) do
+		t = t[v]
 	end
 
-	return tbl
-
+	return t
 end
 
-function RiceLib.table.GetByString(str)
-	local tbl = _G
-	for _,v in ipairs(string.Split(str,".")) do
-		tbl = tbl[v]
-	end
-
-	return tbl
-end
-
-local function doRemove(tbl, value)
+local function remove_by_val(t, value)
 	local instances = {}
 
-	for index, val in ipairs(tbl) do
+	for index, val in ipairs(t) do
 		if val == value then table.insert(instances, index) end
 	end
 
-	if not table.IsEmpty(instances) then table.remove(tbl, instances[1]) end
+	if not table.IsEmpty(instances) then table.remove(t, instances[1]) end
 
 	if #instances > 1 then
-		doRemove(tbl, value)
+		remove_by_val(t, value)
 	end
 end
 
-function RiceLib.table.RemoveByValue(tbl, value)
-	doRemove(tbl, value)
-end
+RiceLib.table = {
+	Inherit = inherit,
+	InheritCopy = inherit_copy
+	GetByString = get_by_str
+	RemoveByValue = remove_by_val
+}
