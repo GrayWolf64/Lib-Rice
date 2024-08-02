b13f712de785e04104478883b91cf4df00c92b1d
RiceMCUT <2950767658@qq.com>
2024-08-02 17:11:11 +0800

    added PoppinsMedium font enable caching for Digi font make panels that has canvas return element from the canvas

--- lua/ricelib/cl_preregister.lua
+++ lua/ricelib/cl_preregister.lua
@@ -50,12 +50,18 @@ RiceUI.Font.Register{
 
 RiceUI.Font.Register{
     CodeName = "Digi",
-    FontName = "DS-Digital"
+    FontName = "DS-Digital",
+    EnableCaching = true
 }
 
 RiceUI.Font.Register{
     CodeName = "VCR",
     FontName = "VCR OSD Mono"
+}
+
+RiceUI.Font.Register{
+    CodeName = "PoppinsMedium",
+    FontName = "Poppins Medium"
 }
 
 RiceLib.VGUI.RegisterColorTheme("Dark1", {Color(40, 40, 40, 255), Color(60, 60, 60, 255), Color(250, 250, 250), Color(60, 60, 60)})
